55d0513ad3be6d23f837af30dc74bf8324d2ca7e
nzpetter <pgolasz@gmail.com>
2019-06-08 15:00:13 +0200

    Misja 17 - Kontrola wymagań formalnych

--- src/app/space/pilot-validators.spec.ts
+++ src/app/space/pilot-validators.spec.ts
@@ -1,7 +1,63 @@
-import { PilotValidators } from './pilot-validators';
+import {PilotValidators} from './pilot-validators';
+import {FormControl} from "@angular/forms";
+import {ajax, AjaxResponse} from "rxjs/ajax";
+import {of} from "rxjs";
 
-describe('PilotValidators', () => {
+fdescribe('PilotValidators', () => {
   it('should create an instance', () => {
     expect(new PilotValidators()).toBeTruthy();
+  });
+
+  describe('pilotName', () => {
+    describe('when value is empty', () => {
+      it('should return null', () => {
+        const control = new FormControl('');
+        expect(PilotValidators.pilotName(control)).toBeNull();
+      });
+    });
+
+    describe('when starts from uppercase letter', () => {
+      it('should return null', () => {
+        const control = new FormControl('Adam');
+        expect(PilotValidators.pilotName(control)).toBeNull();
+      });
+    });
+
+    describe('when starts from lowcase letter', () => {
+      it('should return validation object', () => {
+        const control = new FormControl('adam');
+        expect(PilotValidators.pilotName(control)).toEqual({pilotName: true});
+      });
+    });
+  });
+
+  describe('pilotForbidden', () => {
+    // tutaj testy...
+    describe('when value is empty', () => {
+      it('should return observable with null', () => {
+        const control = new FormControl('');
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toBeNull());
+      });
+    });
+
+    describe('when value is forbidden', () => {
+      it('should return observable with validation object', () => {
+        const control = new FormControl('Ciapciak');
+        spyOn(ajax, 'get').and.returnValue(of({response: [{name: 'Ciapciak'}]} as AjaxResponse));
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toEqual({pilotForbidden: true}));
+      });
+    });
+
+
+    describe('when value is not forbidden', () => {
+      it('should return observable with null', () => {
+        const control = new FormControl('Adama');
+        spyOn(ajax, 'get').and.returnValue(of({response: []} as AjaxResponse));
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toBeNull());
+      });
+    });
   });
 });
